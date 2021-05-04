87c8cbdbb01813af71279ac86c1663779b478c6f
fronz <z233@msn.com>
2021-05-04 14:44:17 +0800

    ♻️ (all): remove Vue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ module.exports = {
   build: {
     rollupOptions: {
       input: {
-        front: resolve(__dirname, 'card.html')
+        front: resolve(__dirname, 'front.html'),
+        back: resolve(__dirname, 'back.html')
       }
     }
   }
