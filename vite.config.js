23632b077b621dcdd484434e9639de30fe25e72b
fronz <z233@msn.com>
2021-05-07 21:37:00 +0800

    👷 (build): minify html

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
 const { resolve } = require('path');
 import { defineConfig } from 'vite';
 import { viteSingleFile } from "vite-plugin-singlefile";
-import { injectHtml } from 'vite-plugin-html';
+import { injectHtml, minifyHtml } from 'vite-plugin-html';
 
 const NODE_ENV = process.env.NODE_ENV;
 
 export default defineConfig({
   plugins: [
     viteSingleFile(), 
-    {
-      ...injectHtml({
-        injectData: {
-          head: NODE_ENV === 'production' ? '' :
-          `<meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">`
-        }
-      })
-    }
+    minifyHtml(),
+    injectHtml({
+      injectData: {
+        head: NODE_ENV === 'production' ? '' :
+        `<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">`
+      }
+    })
   ],
   build: {
     cssCodeSplit: false,
