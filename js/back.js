0d3917e660f4c1841cc33865299b32f4f90c6623
fronz <z233@msn.com>
2021-05-27 22:52:47 +0800

    🎨 (all): improve struture

--- js/back.js
+++ js/back.js
@@ -1,114 +1,130 @@
 import '../style.css';
-import test from './test';
-
-test();
-
-
+import mockAnkidroid from './mock';
 if(void 0===window.Persistence){var _persistenceKey="github.com/SimonLammer/anki-persistence/",_defaultKey="_default";if(window.Persistence_sessionStorage=function(){var e=!1;try{"object"==typeof window.sessionStorage&&(e=!0,this.clear=function(){for(var e=0;e<sessionStorage.length;e++){var t=sessionStorage.key(e);0==t.indexOf(_persistenceKey)&&(sessionStorage.removeItem(t),e--)}},this.setItem=function(e,t){void 0==t&&(t=e,e=_defaultKey),sessionStorage.setItem(_persistenceKey+e,JSON.stringify(t))},this.getItem=function(e){return void 0==e&&(e=_defaultKey),JSON.parse(sessionStorage.getItem(_persistenceKey+e))},this.removeItem=function(e){void 0==e&&(e=_defaultKey),sessionStorage.removeItem(_persistenceKey+e)})}catch(e){}this.isAvailable=function(){return e}},window.Persistence_windowKey=function(e){var t=window[e],i=!1;"object"==typeof t&&(i=!0,this.clear=function(){t[_persistenceKey]={}},this.setItem=function(e,i){void 0==i&&(i=e,e=_defaultKey),t[_persistenceKey][e]=i},this.getItem=function(e){return void 0==e&&(e=_defaultKey),t[_persistenceKey][e]||null},this.removeItem=function(e){void 0==e&&(e=_defaultKey),delete t[_persistenceKey][e]},void 0==t[_persistenceKey]&&this.clear()),this.isAvailable=function(){return i}},window.Persistence=new Persistence_sessionStorage,Persistence.isAvailable()||(window.Persistence=new Persistence_windowKey("py")),!Persistence.isAvailable()){var titleStartIndex=window.location.toString().indexOf("title"),titleContentIndex=window.location.toString().indexOf("main",titleStartIndex);titleStartIndex>0&&titleContentIndex>0&&titleContentIndex-titleStartIndex<10&&(window.Persistence=new Persistence_windowKey("qt"))}}
 
-const UA = navigator.userAgent;
-const isMobile = /Android/i.test(UA);
-const isAndroidWebview = /wv/i.test(UA);
+const isDev = import.meta.env.DEV;
 
-// Mock Anki API
-if (!isMobile) {
-  AnkiDroidJS = {
-    init: function() {},
-    ankiGetNewCardCount: () => 20,
-    ankiGetLrnCardCount: () => 100,
-    ankiGetETA: () => '29'
+if (isDev) mockAnkidroid();
+
+(function (AnkiDroidJS) {
+  const status = parseInt(Persistence.getItem(), 10);
+  const nextButton = document.querySelector('#nextButton');
+  nextButton.onclick = () => {
+    switch (status) {
+      case 4:
+        buttonAnswerEase4();
+        break;
+
+      case 3:
+        buttonAnswerEase3();
+        break;
+
+      case 2:
+        buttonAnswerEase2();
+        break;
+
+      case 1:
+        buttonAnswerEase1();
+        break;
+
+      default:
+        break;
+    }
   };
-  function buttonAnswerEase1() { console.log(Persistence.getItem()); window.location = '/front.html'; };
-  function buttonAnswerEase2() { console.log(Persistence.getItem()); window.location = '/front.html'; };
-  function buttonAnswerEase3() { console.log(Persistence.getItem()); window.location = '/front.html'; };
-  function buttonAnswerEase4() { console.log(Persistence.getItem()); window.location = '/front.html'; };
-  function showAnswer() { window.location = '/back.html' };
-}
 
-const status = parseInt(Persistence.getItem(), 10);
-const nextButton = document.querySelector('#nextButton');
-nextButton.onclick = () => {
-  switch (status) {
-    case 4:
-      buttonAnswerEase4();
-      break;
+  async function initAudio() {
+    const MIN_DURATION = 1000;
 
-    case 3:
-      buttonAnswerEase3();
-      break;
+    const AudioIconAnimation = function (selector) {
+      this.icon = document.querySelector(selector);
+      this.volume1 = this.icon.querySelector('.volume-1');
+      this.volume2 = this.icon.querySelector('.volume-2');
+      this.animations = [];
+      this.start = -1;
+      this.duration = 0;
+    }
 
-    case 2:
-      buttonAnswerEase2();
-      break;
+    AudioIconAnimation.prototype.setDuration = function (duration) {
+      this.duration = duration;
+    }
 
-    case 1:
-      buttonAnswerEase1();
-      break;
+    AudioIconAnimation.prototype.play = function () {
+      this.start = Date.now();
+      this.volume1.style.animation = `play 0.4s steps(4, end) infinite`;
+      this.volume2.style.animation = `play 0.4s steps(2, end) infinite`;
+      setTimeout(() => {
+        this.stop();
+      }, this.duration * 1000);
+    }
 
-    default:
-      break;
+    AudioIconAnimation.prototype.stop = function () {
+      const now = Date.now();
+      const end = this.start + MIN_DURATION;
+      const stopAnimation = () => {
+        this.volume1.style.animation = '';
+        this.volume2.style.animation = '';
+        this.start = -1;
+      };
+
+      if (now >= end) stopAnimation();
+      else {
+        setTimeout(() => {
+          stopAnimation();
+        }, end - now);
+      }
+    }
+
+    const AudioCommand = function (wrap, animation) {
+      this.wrap = wrap;
+      this.audioElement = null;
+      this.audioButton = null;
+      this.animation = animation;
+    }
+
+    AudioCommand.prototype.init = function () {
+      this.audioButton = document.querySelectorAll(this.wrap + ' > .replaybutton')[0];
+      this.audioElement = document.createElement('audio');
+      this.audioElement.preload = 'metadata';
+      this.audioElement.src = this.audioButton.href.replace('playsound:', '');
+    }
+
+    AudioCommand.prototype.play = function () {
+      if (!isDev) this.audioButton.dispatchEvent(new MouseEvent('click'));
+      if (isDev) this.audioElement.play();
+      this.animation.play();
+    }
+
+    const pronIconAnimation = new AudioIconAnimation('.pronounceIcon');
+    const sentIconAnimation = new AudioIconAnimation('.sentenceIcon');
+
+    const setPlayCommand = function (button, command) {
+      button.onclick = function () {
+        command.play();
+      }
+    }
+
+    function getAudioDuration(audioElement) {
+      return new Promise(resolve => {
+        audioElement.addEventListener('loadeddata', () => {
+          resolve(audioElement.duration);
+        });
+      });
+    };
+
+    // Pronouce Audio
+    const playPronounceButton = document.querySelector('#playPronounceButton');
+    const pronounceAudio = new AudioCommand('.pronAudioWarp', pronIconAnimation);
+    pronounceAudio.init();
+    setPlayCommand(playPronounceButton, pronounceAudio);
+    pronIconAnimation.setDuration(await getAudioDuration(pronounceAudio.audioElement));
+    pronounceAudio.play();
+
+    // Sentence Audio
+    const sentenceIcon = document.querySelector('.sentenceIcon');
+    const sentenceAudio = new AudioCommand('.sentenAudioWarp', sentIconAnimation);
+    sentenceAudio.init();
+    setPlayCommand(sentenceIcon, sentenceAudio);
+    sentIconAnimation.setDuration(await getAudioDuration(sentenceAudio.audioElement));
   }
-};
-
-// Init Pronounce Audio
-const pronReplaybutton = document.querySelectorAll('.pronAudioWarp > .replaybutton')[0];
-const pronounceAudio = document.createElement('audio');
-
-if (isMobile) pronounceAudio.src = pronReplaybutton.href.replace('playsound:', '');
-
-const PRONOUNCE_ANIMATION_TIME = 1.2;
-let audioTimeout = -1;
-
-playPronounce();
-
-function playPronounce() {
-  document.querySelector('.pronAudioWarp').children[0].dispatchEvent(new MouseEvent('click'));
-  // Animation
-  const volume1 = document.querySelectorAll('.pronounceIcon > .volume-1')[0];
-  const volume2 = document.querySelectorAll('.pronounceIcon > .volume-2')[0];
-
-  setAudioAnimation(volume1, volume2, PRONOUNCE_ANIMATION_TIME);
-};
-
-// Init Sentence Audio
-const sentenReplaybutton = document.querySelectorAll('.sentenAudioWarp > .replaybutton')[0];
-const sentenceAudio = document.createElement('audio');
-if (isMobile) sentenceAudio.src = sentenReplaybutton.href.replace('playsound:', '');
-const sentenceAudioDuration = sentenceAudio.duration;
-
-const SENTENCE_ANIMATION_TIME = sentenceAudioDuration / 1000;
-
-function playSentence() {
-  document.querySelector('.sentenAudioWarp').children[0].dispatchEvent(new MouseEvent('click'));
-  // sentenceAudio.play();
-  // Animation
-  const volume1 = document.querySelectorAll('.sentenceIcon > .volume-1')[0];
-  const volume2 = document.querySelectorAll('.sentenceIcon > .volume-2')[0];
-
-  setAudioAnimation(volume1, volume2, SENTENCE_ANIMATION_TIME);
-}
-
-function setAudioAnimation(volume1, volume2, duration) {
-
-  volume1.style.animation = `play 0.4s steps(4, end) ${parseInt(duration / 0.4, 10)}`;
-  volume2.style.animation = `play 0.4s steps(2, end) ${parseInt(duration / 0.4, 10)}`;
-
-  setTimeout(() => {
-    clearAudioAnimation(volume1, volume2);
-  }, duration * 1000);
-}
-
-function clearAudioAnimation(volume1, volume2) {
-  volume1.style.animation = '';
-  volume2.style.animation = '';
-};
-
-const playPronounceButton = document.querySelector('#playPronounceButton');
-playPronounceButton.onclick = () => {
-  playPronounce();
-};
-
-const sentenceIcon = document.querySelector('.sentenceIcon');
-sentenceIcon.onclick = () => {
-  playSentence();
-};
+  initAudio();
+})(window.AnkiDroidJS)
--- js/front.js
+++ js/front.js
@@ -1,47 +1,114 @@
 import '../style.css';
-import { AudioIconAnimation, AudioCommand } from './utils';
 import mockAnkidroid from './mock';
 if(void 0===window.Persistence){var _persistenceKey="github.com/SimonLammer/anki-persistence/",_defaultKey="_default";if(window.Persistence_sessionStorage=function(){var e=!1;try{"object"==typeof window.sessionStorage&&(e=!0,this.clear=function(){for(var e=0;e<sessionStorage.length;e++){var t=sessionStorage.key(e);0==t.indexOf(_persistenceKey)&&(sessionStorage.removeItem(t),e--)}},this.setItem=function(e,t){void 0==t&&(t=e,e=_defaultKey),sessionStorage.setItem(_persistenceKey+e,JSON.stringify(t))},this.getItem=function(e){return void 0==e&&(e=_defaultKey),JSON.parse(sessionStorage.getItem(_persistenceKey+e))},this.removeItem=function(e){void 0==e&&(e=_defaultKey),sessionStorage.removeItem(_persistenceKey+e)})}catch(e){}this.isAvailable=function(){return e}},window.Persistence_windowKey=function(e){var t=window[e],i=!1;"object"==typeof t&&(i=!0,this.clear=function(){t[_persistenceKey]={}},this.setItem=function(e,i){void 0==i&&(i=e,e=_defaultKey),t[_persistenceKey][e]=i},this.getItem=function(e){return void 0==e&&(e=_defaultKey),t[_persistenceKey][e]||null},this.removeItem=function(e){void 0==e&&(e=_defaultKey),delete t[_persistenceKey][e]},void 0==t[_persistenceKey]&&this.clear()),this.isAvailable=function(){return i}},window.Persistence=new Persistence_sessionStorage,Persistence.isAvailable()||(window.Persistence=new Persistence_windowKey("py")),!Persistence.isAvailable()){var titleStartIndex=window.location.toString().indexOf("title"),titleContentIndex=window.location.toString().indexOf("main",titleStartIndex);titleStartIndex>0&&titleContentIndex>0&&titleContentIndex-titleStartIndex<10&&(window.Persistence=new Persistence_windowKey("qt"))}}
 
 const isDev = import.meta.env.DEV;
+
 if (isDev) mockAnkidroid();
 
-(async function(AnkiDroidJS) {
+(function (AnkiDroidJS) {
 
   // Init Ankidroid Javascript API
-  AnkiDroidJS.init(JSON.stringify({"version" : "0.0.1", "developer" : "dev@mail.com"}));
+  AnkiDroidJS.init(JSON.stringify({
+    "version": "0.0.1",
+    "developer": "dev@mail.com"
+  }));
 
-  const pronIconAnimation = new AudioIconAnimation('.pronounceIcon');
-  const sentIconAnimation = new AudioIconAnimation('.sentenceIcon');
+  async function initAudio() {
+    const MIN_DURATION = 1000;
 
-  const setPlayCommand = function(button, command) {
-    button.onclick = function() {
-      command.play();
+    const AudioIconAnimation = function (selector) {
+      this.icon = document.querySelector(selector);
+      this.volume1 = this.icon.querySelector('.volume-1');
+      this.volume2 = this.icon.querySelector('.volume-2');
+      this.animations = [];
+      this.start = -1;
+      this.duration = 0;
     }
-  }
 
-  function getAudioDuration(audioElement) {
-    return new Promise(resolve => {
-      audioElement.addEventListener('loadeddata', () => {
-        resolve(audioElement.duration);
+    AudioIconAnimation.prototype.setDuration = function (duration) {
+      this.duration = duration;
+    }
+
+    AudioIconAnimation.prototype.play = function () {
+      this.start = Date.now();
+      this.volume1.style.animation = `play 0.4s steps(4, end) infinite`;
+      this.volume2.style.animation = `play 0.4s steps(2, end) infinite`;
+      setTimeout(() => {
+        this.stop();
+      }, this.duration * 1000);
+    }
+
+    AudioIconAnimation.prototype.stop = function () {
+      const now = Date.now();
+      const end = this.start + MIN_DURATION;
+      const stopAnimation = () => {
+        this.volume1.style.animation = '';
+        this.volume2.style.animation = '';
+        this.start = -1;
+      };
+
+      if (now >= end) stopAnimation();
+      else {
+        setTimeout(() => {
+          stopAnimation();
+        }, end - now);
+      }
+    }
+
+    const AudioCommand = function (wrap, animation) {
+      this.wrap = wrap;
+      this.audioElement = null;
+      this.audioButton = null;
+      this.animation = animation;
+    }
+
+    AudioCommand.prototype.init = function () {
+      this.audioButton = document.querySelectorAll(this.wrap + ' > .replaybutton')[0];
+      this.audioElement = document.createElement('audio');
+      this.audioElement.preload = 'metadata';
+      this.audioElement.src = this.audioButton.href.replace('playsound:', '');
+    }
+
+    AudioCommand.prototype.play = function () {
+      if (!isDev) this.audioButton.dispatchEvent(new MouseEvent('click'));
+      if (isDev) this.audioElement.play();
+      this.animation.play();
+    }
+
+    const pronIconAnimation = new AudioIconAnimation('.pronounceIcon');
+    const sentIconAnimation = new AudioIconAnimation('.sentenceIcon');
+
+    const setPlayCommand = function (button, command) {
+      button.onclick = function () {
+        command.play();
+      }
+    }
+
+    function getAudioDuration(audioElement) {
+      return new Promise(resolve => {
+        audioElement.addEventListener('loadeddata', () => {
+          resolve(audioElement.duration);
+        });
       });
-    });
-  };
+    };
 
-  // Pronouce Audio
-  const playPronounceButton = document.querySelector('#playPronounceButton');
-  const pronounceAudio = new AudioCommand('.pronAudioWarp', pronIconAnimation);
-  pronounceAudio.init();
-  setPlayCommand(playPronounceButton, pronounceAudio);
-  pronIconAnimation.setDuration(await getAudioDuration(pronounceAudio.audioElement));
-  pronounceAudio.play();
+    // Pronouce Audio
+    const playPronounceButton = document.querySelector('#playPronounceButton');
+    const pronounceAudio = new AudioCommand('.pronAudioWarp', pronIconAnimation);
+    pronounceAudio.init();
+    setPlayCommand(playPronounceButton, pronounceAudio);
+    pronIconAnimation.setDuration(await getAudioDuration(pronounceAudio.audioElement));
+    pronounceAudio.play();
 
-  // Sentence Audio
-  const sentenceIcon = document.querySelector('.sentenceIcon');
-  const sentenceAudio = new AudioCommand('.sentenAudioWarp', sentIconAnimation);
-  sentenceAudio.init();
-  setPlayCommand(sentenceIcon, sentenceAudio);
-  sentIconAnimation.setDuration(await getAudioDuration(sentenceAudio.audioElement));
+    // Sentence Audio
+    const sentenceIcon = document.querySelector('.sentenceIcon');
+    const sentenceAudio = new AudioCommand('.sentenAudioWarp', sentIconAnimation);
+    sentenceAudio.init();
+    setPlayCommand(sentenceIcon, sentenceAudio);
+    sentIconAnimation.setDuration(await getAudioDuration(sentenceAudio.audioElement));
+  }
+  initAudio();
 
   // Answer Button
   const answerButtons = document.querySelectorAll('.answerButton');
@@ -65,6 +132,7 @@ if (isDev) mockAnkidroid();
   const buttonGroup1 = document.querySelector('#buttonGroup1');
   const buttonGroup2 = document.querySelector('#buttonGroup2');
   const sentenceHint = document.querySelector('#sentenceHint');
+  const sentenceIcon = document.querySelector('.sentenceIcon');
 
   showHintButton.onclick = () => {
     toggleElementDisplay(buttonGroup1);
@@ -81,8 +149,8 @@ if (isDev) mockAnkidroid();
   };
 
   // Count
-  const newCardCount  = document.querySelector('#newCardCount');
-  const learnCardCount  = document.querySelector('#learnCardCount');
+  const newCardCount = document.querySelector('#newCardCount');
+  const learnCardCount = document.querySelector('#learnCardCount');
   const ETA = document.querySelector('#ETA');
 
   newCardCount.innerText = AnkiDroidJS.ankiGetNewCardCount();
